607965795d44e7c132e6295a6f4a24e7adc853bc
av-dev <vereschak@gmail.com>
2020-04-13 21:37:23 +0300

    (ci): fix config

--- test/connection-configs.js
+++ test/connection-configs.js
@@ -5,12 +5,13 @@ exports.postgres = {
   database: process.env.POSTGRES_DATABASE || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'postgres',
   user: process.env.POSTGRES_USER || 'postgres',
-  port: process.env.POSTGRES_PORT || 5432,
+  port: process.env.POSTGRES_PORT || 25432,
 };
 
 exports.mysql = {
   host: process.env.MYSQL_HOST || 'localhost',
   password: process.env.MYSQL_PASSWORD || 'mysql',
+  database: process.env.MYSQL_DATABASE || 'mysql',
   user: process.env.MYSQL_USER || 'root',
-  port: process.env.MYSQL_PORT || 3306,
+  port: process.env.MYSQL_PORT || 23306,
 };
